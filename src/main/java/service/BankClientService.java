03784970b006abcb510ecff3f744977044bac611
eurusov <eurusov@users.noreply.github.com>
2019-09-07 00:31:03 +0300

    Small cleanup

--- src/main/java/service/BankClientService.java
+++ src/main/java/service/BankClientService.java
@@ -1,5 +1,7 @@
 package service;
 
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
 import dao.BankClientDAO;
 import exception.DBException;
 import model.BankClient;
@@ -15,7 +17,8 @@ public class BankClientService {
     public BankClientService() {
     }
 
-    public BankClient getClientById(long id) throws DBException {
+    public @Nullable
+    BankClient getClientById(long id) throws DBException {
         try (BankClientDAO dao = getBankClientDAO()) {
             return dao.getClientById(id);
         } catch (SQLException e) {
@@ -24,13 +27,14 @@ public class BankClientService {
     }
 
     /**
-     * Возвращает клиента по его имени, или null если такого клиента нет.
+     * Возвращает клиента по его имени, или <code>null</code> если такого клиента нет.
      *
      * @param name имя клиента
-     * @return объект BankClient, или null если такого клиента нет
-     * @throws DBException если во время выполнения запроса было выброшено SQLException
+     * @return объект <code>BankClient</code>, или <code>null</code> если такого клиента нет
+     * @throws DBException если во время выполнения запроса было выброшено <code>SQLException</code>
      */
-    public BankClient getClientByName(String name) throws DBException {
+    public @Nullable
+    BankClient getClientByName(String name) throws DBException {
         try (BankClientDAO dao = getBankClientDAO()) {
             return dao.getClientByName(name);
         } catch (SQLException e) {
@@ -41,10 +45,11 @@ public class BankClientService {
     /**
      * Возвращает список всех клиентов в таблице или пустой список, если клиентов нет.
      *
-     * @return List of BankClient objects.
-     * @throws DBException если во время выполнения запроса было выброшено SQLException
+     * @return <code>List</code> of <code>BankClient</code> objects.
+     * @throws DBException если во время выполнения запроса было выброшено <code>SQLException</code>
      */
-    public List<BankClient> getAllClient() {
+    public @NotNull
+    List<BankClient> getAllClient() {
         try (BankClientDAO dao = getBankClientDAO()) {
             return dao.getAllBankClient();
         } catch (SQLException e) {
@@ -56,8 +61,8 @@ public class BankClientService {
      * Удаляет клиента с данным именем из таблицы.
      *
      * @param name имя клиента
-     * @return true в случае успеха, false в случае если такой клиент отсутствует в таблице
-     * @throws DBException если во время выполнения запроса было выброшено SQLException или IllegalStateException
+     * @return <code>true</code> в случае успеха, <code>false</code> в случае если такой клиент отсутствует в таблице
+     * @throws DBException если во время выполнения запроса было выброшено <code>SQLException</code> или <code>IllegalStateException</code>
      */
     public boolean deleteClient(String name) {
         if (getClientByName(name) == null) {
@@ -74,9 +79,9 @@ public class BankClientService {
     /**
      * Добавляет клиента в таблицу.
      *
-     * @param client объект BankClient
-     * @return true в случае успеха, false в случае если такой клиент уже был
-     * @throws DBException если во время выполнения запроса было выброшено SQLException или IllegalStateException
+     * @param client объект <code>BankClient</code>
+     * @return <code>true</code> в случае успеха, <code>false</code> в случае если такой клиент уже есть в таблице
+     * @throws DBException если во время выполнения запроса было выброшено <code>SQLException</code> или <code>IllegalStateException</code>
      */
     public boolean addClient(BankClient client) throws DBException {
         if (getClientByName(client.getName()) != null) {
@@ -96,7 +101,7 @@ public class BankClientService {
      * @param sender клиент от которого переводятся деньги
      * @param name   имя клиента которому переводятся деньги
      * @param value  сумма перевода
-     * @return <tt>true</tt> в случае успеха
+     * @return <code>true</code> в случае успеха
      * @throws DBException если во время выполнения запроса было выброшено
      *                     <code>SQLException</code> или <code>IllegalStateException</code>
      */
@@ -106,15 +111,12 @@ public class BankClientService {
             return false;
         }
         try (BankClientDAO dao = getBankClientDAO()) {
-//            getBankClientDAO().doMoneyTransfer(sender, recipient, value);
             if (dao.validateClient(sender.getName(), sender.getPassword())
                     && dao.isClientHasSum(sender.getName(), value)
             ) {
-//            connection.setAutoCommit(false);
                 dao.updateClientsMoney(sender.getName(), sender.getPassword(), -value);
                 dao.updateClientsMoney(name, recipient.getPassword(), value);
                 return true;
-//            connection.commit();
             } else {
                 return false;
             }
@@ -163,9 +165,6 @@ public class BankClientService {
         }
     }
 
-    /**
-     * Return existing BankClientDAO instance
-     */
     private BankClientDAO getBankClientDAO() {
         return new BankClientDAO(getMysqlConnection());
     }
